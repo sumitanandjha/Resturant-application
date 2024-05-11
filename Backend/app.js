a781adad2e4c5c3e7e89f28d2ea8c630a94e187e
Sumit Anand Jha <sumitsjce16@gmail.com>
2024-05-11 16:46:10 +0530

    production fixes

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from "path";
 import { dbConnection } from  "./database/dbconnection.js";
 import { errorMiddleware } from './error/error.js';
 import reservationRouter from './routes/reservationRoute.js';
@@ -8,8 +9,12 @@ import reservationRouter from './routes/reservationRoute.js';
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
+const __dirname = path.resolve();
+
+
+
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:3000',
     methods: ["POST"],
     credentials: true,
 }));
@@ -20,5 +25,11 @@ app.use(express.urlencoded({ extended: true })); // Corrected 'extended'
 dbConnection();
 app.use(errorMiddleware);
 app.use('/api/v1/reservation', reservationRouter);
+
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Frontend/dist/index.html"));
+})
 
 export default app;
